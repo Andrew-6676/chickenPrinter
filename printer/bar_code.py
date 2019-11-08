14c1bc9c19a9e443d5ef3ed86fdafe9d60225726
Andrew <andrew-6676@ya.ru>
2019-11-09 00:19:14 +0300

    feat: add functions for printing from template

---
+++ printer/bar_code.py
@@ -0,0 +1,58 @@
+import time
+from io import BytesIO
+import barcode
+from PIL import Image as PILImage
+import openpyxl
+import treepoem
+from barcode.writer import ImageWriter
+
+# ----------------------------------------------------------------------------------------------- #
+def generateBarCode_simple(type, data):
+	fp = BytesIO()
+	writer_options = {
+        'module_width': 0.08,
+        'module_height': 1.4,
+        'quiet_zone': 0.1,
+        'font_size': 5,
+        'text_distance': 0.7,
+	}
+
+	barcode.generate(type, data,
+	                 writer=ImageWriter(),
+	                 output=fp,
+	                 writer_options=writer_options)
+	return PILImage.open(fp)
+# ----------------------------------------------------------------------------------------------- #
+def generateBarCode_tree(type, data):
+	options = {
+		'ean13': {
+			'includetext': True,
+			'textfont': 'Arial-Black',
+			'textsize': 10,
+			# 'textgaps': 5,
+			'height': 0.4,
+		},
+		'code128': {
+			'includetext':True,
+			'textfont': 'Arial-Black',
+			'textsize': 10,
+			'textgaps': 2,
+			'height': 0.3,
+			'includecheck': True
+		}
+	}
+	image = treepoem.generate_barcode(
+		barcode_type=type,
+		data=data,
+		options = options[type]
+	)
+	size = image.width/2, image.width/2
+	image.thumbnail(size, PILImage.NEAREST)
+	# image.convert('1').save('barcode.png')
+	return image
+# ----------------------------------------------------------------------------------------------- #
+def insertBarCode(sheet, anchor, code_type, code_data):
+	im = generateBarCode_tree(code_type, code_data)
+	img = openpyxl.drawing.image.Image(im)
+	img.anchor = anchor
+	sheet.add_image(img)
---
+++ printer/printer.py
@@ -0,0 +1,129 @@
+import os
+import subprocess
+import time
+import re
+import winsound
+
+import openpyxl
+from openpyxl.utils.cell import get_column_letter
+import win32print
+import win32com.client
+
+from bar_code import insertBarCode
+
+# ----------------------------------------------------------------------------------------------- #
+def beep():
+	frequency = 2500  # Set Frequency To 2500 Hertz
+	duration = 100  # Set Duration To 1000 ms == 1 second
+	winsound.Beep(frequency, duration)
+	winsound.Beep(frequency, duration)
+	winsound.Beep(frequency, duration)
+# ----------------------------------------------------------------------------------------------- #
+def xlsx_to_pdf(src_file):
+	"""
+	конвертируем XLSX в PDF
+	:param src_file: путь к конвертируемому файлу
+	:return: путь к получившемуся pdf-файлу
+	"""
+	current_work_dir = os.getcwd()
+	excel = win32com.client.Dispatch("Excel.Application")
+	excel.Visible = False
+	wb_path = src_file
+	wb = excel.Workbooks.Open(wb_path)
+
+	ws_index_list = [1]  # say you want to print these sheets
+
+	path_to_pdf = current_work_dir + '/tmp/' + 'print.pdf'
+
+	wb.WorkSheets(ws_index_list).Select()
+	wb.ActiveSheet.ExportAsFixedFormat(0, path_to_pdf)
+
+	wb.Close()
+
+	return path_to_pdf
+	# excel.Quit()
+
+# ----------------------------------------------------------------------------------------------- #
+def genereate_file_to_print(template_file, data):
+	"""
+	Подстановка значений из data в шаблон и конвертирование его в PDF
+	:param data: данные для вставки в шаблон
+	:return:
+	"""
+	current_work_dir = os.getcwd()
+
+	wb = openpyxl.load_workbook(template_file)
+	sheet = wb.active
+	amountOfRows = sheet.max_row
+	amountOfColumns = sheet.max_column
+
+	# проходимся по всем ячейкам, заменяем шаблон на значения
+	for i in range(amountOfColumns):
+		for k in range(amountOfRows):
+			cell = str(sheet[get_column_letter(i + 1) + str(k + 1)].value)
+			if cell != 'None':
+				newCell = cell
+				for v in data:
+					newCell = re.sub(r'\{\{' + v + '\}\}', data[v], newCell)
+				sheet[get_column_letter(i + 1) + str(k + 1)] = newCell
+				# print(cell, '-', newCell)
+
+				# ищем штрихкод
+				bar_res = re.findall(r'\{\{(.+)=(ean13|code128)\}\}', newCell)
+				if bar_res:
+					sheet[get_column_letter(i + 1) + str(k + 1)] = '<code>'
+					code_data, code_type = bar_res[0]
+					code_data = data[code_data]
+					# print('-->', get_column_letter(i + 1) + str(k + 1), code_type, code_data)
+					insertBarCode(sheet, get_column_letter(i + 1) + str(k + 1), code_type, code_data)
+
+	wb.save('./tmp/print.xlsx')
+
+	return current_work_dir + '/tmp/print.xlsx'
+# ----------------------------------------------------------------------------------------------- #
+def print_file(pdf_file_name):
+	"""
+	Выводим на принтер pdf-файл
+	:param pdf_file_name:
+	:return:
+	"""
+	print('---> pdf', pdf_file_name)
+	# GHOSTSCRIPT_PATH = "w:\\#### D\Work\\# python\\tsc_sample\\gsprint\\gsprint.exe"
+	# GSPRINT_PATH = "C:\\Program Files (x86)\\gs\\gs9.50\\bin\\gswin32.exe"
+	#
+	printer_name = win32print.GetDefaultPrinter()
+	print(f'printer - [{printer_name}]')
+
+	#
+	# #win32api.ShellExecute (0, "print", filename, f'"{win32print.GetDefaultPrinter()}"', ".", 0)
+	# win32api.ShellExecute(0, 'open', GSPRINT_PATH, '-ghostscript "'+GHOSTSCRIPT_PATH+'" -printer "'+printer_name+'" "1.pdf"', '.', 0)
+
+	args = 'gswin32c.exe ' \
+	       '-sDEVICE=mswinpr2 ' \
+	       '-dBATCH ' \
+	       '-dNOPAUSE ' \
+	       '-dFitPage ' \
+	       '-sOutputFile="%printer%{}" '.format(printer_name)
+	ghostscript = args + f'"{pdf_file_name}"'
+	subprocess.call(ghostscript, shell=True)
+# ----------------------------------------------------------------------------------------------- #
+
+data = {
+	'energy': '<ENERGY>',
+	'name': '<NAME>',
+	'barcode1': '2150000000017',
+	'barcode2': '2210735005000511192'
+}
+
+t = time.time()
+
+x = genereate_file_to_print('./templates/template.xlsx', data)
+print(time.time() - t)
+t = time.time()
+p = xlsx_to_pdf(x)
+print(time.time() - t)
+t = time.time()
+print_file(p)
+print(time.time() - t)
+beep()
+print('---------------')
